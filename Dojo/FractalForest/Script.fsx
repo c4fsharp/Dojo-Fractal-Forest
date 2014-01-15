ac160b1d026afb0c277aca568221e239791f0236
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-01-14 23:15:45 -0800

    Base script added.

---
+++ Dojo/FractalForest/Script.fsx
@@ -0,0 +1,50 @@
+﻿open System
+open System.Drawing
+open System.Windows.Forms
+
+// Create a form to display the graphics
+let width, height = 500, 500         
+let form = new Form(Width = width, Height = height)
+let box = new PictureBox(BackColor = Color.White, Dock = DockStyle.Fill)
+let image = new Bitmap(width, height)
+let graphics = Graphics.FromImage(image)
+let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
+
+box.Image <- image
+form.Controls.Add(box) 
+
+// Compute the endpoint of a line
+// starting at x, y, going at a certain angle
+// for a certain length. 
+let endpoint x y angle length =
+    x + length * cos angle,
+    y + length * sin angle
+
+let flip x = (float)height - x
+
+// Utility function: draw a line of given width, 
+// starting from x, y
+// going at a certain angle, for a certain length.
+let drawLine (target : Graphics) (brush : Brush) 
+             (x : float) (y : float) 
+             (angle : float) (length : float) (width : float) =
+    let x_end, y_end = endpoint x y angle length
+    let origin = new PointF((single)x, (single)(y |> flip))
+    let destination = new PointF((single)x_end, (single)(y_end |> flip))
+    let pen = new Pen(brush, (single)width)
+    target.DrawLine(pen, origin, destination)
+
+let draw x y angle length width = 
+    drawLine graphics brush x y angle length width
+
+let pi = Math.PI
+
+// Now... your turn to draw
+// The trunk
+draw 250. 50. (pi*(0.5)) 100.0 4.
+let x, y = endpoint 250. 50. (pi*(0.5)) 100.
+// first and second branches
+draw x y (pi*(0.5 + 0.3)) 50. 2.
+draw x y (pi*(0.5 - 0.4)) 50. 2.
+
+form.ShowDialog()
